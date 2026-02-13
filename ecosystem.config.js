ded0e0053e4f5d13fa638861237fd640b1a44897
Jose David <josedsg@gmail.com>
2026-02-13 13:06:40 -0600

    Actualizacion del pm2 ecosistema

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,16 @@ module.exports = {
         {
             name: "rt_operaciones",
             script: "npm",
-            args: "run dev", // IMPORTANTE: Usa 'start', no 'run dev'
+            args: "start", // IMPORTANTE: Usa 'start', no 'run dev'
             env: {
                 NODE_ENV: "production",
                 // Si usas NextAuth, añade esta línea:
-                NEXTAUTH_URL: "https://rt-dev.neosyscr.com",
+                NEXTAUTH_URL: "https://operaciones.riotapezco.net",
                 // Si usas variables personalizadas para redirecciones:
-                NEXT_PUBLIC_BASE_URL: "https://rt-dev.neosyscr.com"
+                NEXT_PUBLIC_BASE_URL: "https://operaciones.riotapezco.net",
+                // AUTH config
+                AUTH_SECRET: "Ew883WM/6mXZwX5XYdD1+T9ehs2Pzx9FJipBQjrTkxQ=",
+                AUTH_TRUST_HOST: "true"
             },
         },
     ],
